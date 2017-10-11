b0b8f4548c6bf7155802f9bda7a58f2012368535
seeyaz <seeyazii@hotmail.com>
2017-10-11 20:08:43 +0000

    added motd and changed apache version

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,8 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #Install apache package
+
+if node['platform_family'] == "rhel"
+  package = "httpd"
+elseif node['platform_family'] == "debian"
+  package = "apache2"
+end
+
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name package
   action :install
 end
 
@@ -13,5 +21,4 @@ service 'apache2' do
   service_name 'httpd'
   action [:start, :enable]
 end
-
 
